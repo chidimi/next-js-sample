5c5f75452b706e4d4c8cf9dc904121e8f17bd2c4
chidimi <daichi.imai@hotmail.com>
2020-10-25 18:40:46 +0900

    modiefide next link import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useAuthentication } from '../hooks/authentication'
-import Link from 'next/Link'
+import React from 'react'
+import Link from 'next/dist/client/link'
 
 export default function Home() {
   const { user } = useAuthentication()
